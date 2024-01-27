9339fde4c57bb69e4638a98b128d420f98d4fcfd
mtthw <matbalinton@gmail.com>
2024-01-27 10:52:04 +0800

    Add action and event handler

--- home.php
+++ home.php
@@ -17,6 +17,36 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+<style>
+  table {
+    width: 80%;
+  }
+  th {
+    text-align: center;
+  }
+  .parent {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+  .parent form {
+    margin: 0px 20px;
+  }
+  .parent form input[type= "submit"]{
+    padding: 0px 15px;
+  }
+    #deleteId {
+    background-color: red;
+    color: white;
+    border: none;
+  }
+  #updateId {
+    background-color: green;
+    color: white;
+    border: none;
+  }
+</style>
 <body>
         <h1><?php echo $username ?></h1>
         <a href="/CRUD-Login/samples/logout.php">Logout</a>   
@@ -27,6 +57,7 @@
       <th scope="col">ID</th>
       <th scope="col">Username</th>
       <th scope="col">Password</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
@@ -34,6 +65,12 @@
       <th scope="row">1</th>
       <td>Mark</td>
       <td>Otto</td>
+      <td>
+        <div class= "parent">
+        <form action="#"><input type="submit" value="Update" name = "update" id = "updateId"></form>
+        <form action="#"><input type="submit" value="Delete" name = "delete" id = "deleteId"></form>
+        </div>
+      </td>
     </tr>
     <tr>
       <th scope="row">2</th>
